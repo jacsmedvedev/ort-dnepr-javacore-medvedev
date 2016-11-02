8adf9ae2c1b88433021bf820bbae9590f59c40ef
Yakov Medvedev <medvedev.teror@gmail.com>
2016-11-02 22:30:43 +0200

    Проверка Кириллицы

--- src/medvedev_Homework_7_arrays_tasks/Medvedev_Cyrillic_check.java
+++ src/medvedev_Homework_7_arrays_tasks/Medvedev_Cyrillic_check.java
@@ -3,8 +3,6 @@ package medvedev_Homework_7_arrays_tasks;
 public class Medvedev_Cyrillic_check {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("йцукенгшщзхъфывапролджэячсмитьбю");
 	}
-
 }
