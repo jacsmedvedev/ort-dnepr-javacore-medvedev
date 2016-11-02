3ea767ad4b7f7a984c16784716c8e9f667877a0d
Yakov Medvedev <medvedev.teror@gmail.com>
2016-11-02 22:36:46 +0200

    Delete Medvedev_Cyrillic_check.java

--- src/medvedev_Homework_7_arrays_tasks/Medvedev_Cyrillic_check.java
+++
@@ -1,9 +0,0 @@
-package medvedev_Homework_7_arrays_tasks;
-
-public class Medvedev_Cyrillic_check {
-
-	public static void main(String[] args) {
-		System.out.print("йцукенгшщзхъфывапролджэячсмитьбю");
-		System.out.print("фффффффффффффыыыыыыыыыыыыыввввввввч");
-	}
-}
