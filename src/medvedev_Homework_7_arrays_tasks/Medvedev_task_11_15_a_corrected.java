cb7bceed2123a92772c8834ede744237c387c4ba
Yakov Medvedev <medvedev.teror@gmail.com>
2016-11-02 23:00:17 +0200

    11.15 а) Исправленное
    Дан массив. Составить программу:
     а) расчета квадратного корня из любого элемента массива

---
+++ src/medvedev_Homework_7_arrays_tasks/Medvedev_task_11_15_a_corrected.java
@@ -0,0 +1,30 @@
+package medvedev_Homework_7_arrays_tasks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Medvedev_task_11_15_a_corrected {
+
+		public static void main(String[] args) {
+
+			int[] array = new int[20];
+			int i;
+			for (i = 0; i < array.length; ++i) {
+				int x = (int) (Math.random() * 100);
+				array[i] = x;
+			}
+
+			System.out.println(Arrays.toString(array));
+			Scanner x = new Scanner(System.in);
+			System.out.println("Введите число от 1 до 20, "
+					+ "\nсоответсвующее индексу элемента массива --- ");
+			int numOfDigit = x.nextInt();
+			if (1 <= numOfDigit && numOfDigit <= 20) {
+			double zzz = Math.sqrt(array[numOfDigit-1]);
+			System.out.println(zzz);
+			} else {
+				System.out.println("Вы ввели недопустимое значение. Попробуйте снова.");
+				
+			}
+		}
+	}
