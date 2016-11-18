9f05df89416fd6b28cf0bde08c15b6f34523bef5
Admin <Admin@Medvedev>
2016-11-18 15:29:05 +0200

    9.10
    Переделал из ифов в массивы. Многое взял с примера Насти.

--- src/medvedev_Homework_7_arrays_tasks/Medvedev_task_9_10.java
+++ src/medvedev_Homework_7_arrays_tasks/Medvedev_task_9_10.java
@@ -1,6 +1,5 @@
 package medvedev_Homework_7_arrays_tasks;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /*
@@ -10,35 +9,36 @@ import java.util.Arrays;
  * 
  */
 
-
+//9.10 Даны названия трех городов.
+// Вывести на экран самое длинное и самое короткое название.
 
 public class Medvedev_task_9_10 {
 
-	// 9.10 Даны названия трех городов.
-	// Вывести на экран самое длинное и самое короткое название.
-
 	public static void main(String[] args) {
-
-		String[] cities = new String[] { "Днепр", "Киев", "Чоп", "Ивано-Франковск", "Токмак" };
-
-	}
-
-	private static void bubbleSort(String[] cities) {
-
-		for (int i = cities.length; i > 0; --i) {
-			for (int j = 0; j < i; ++j) {
-				if (cities[j].charAt(0) > cities[j + 1].charAt(0)) {
-
-					String temp = cities[j];
-					cities[j] = cities[j + 1];
-					cities[j + 1] = temp;
-				}
-			} System.out.println(Arrays.toString(cities));
-
-			// System.out.println(cities[0] + " - имеет меньше всего букв.");
-			// System.out.println(cities[cities.length-1] + " - имеет больше
-			// всего букв.");
+		String[] cities = new String[] { "Днепр", "Киев", "Чоп", "Ивано-Франковск", "Токмак",  };
+		System.out.println(Arrays.toString(cities));
+		int maxLetters = cities[0].length();
+		for (int i = 0; i < cities.length; ++i) {
+			if (maxLetters < cities[i].length()) {
+				maxLetters = cities[i].length();
+			}
 		}
-		
-	} 
+		for (int i = 0; i < cities.length; i++) {
+			if (cities[i].length() == maxLetters) {
+				System.out.println(cities[i] + " - имеет больше всего букв.");
+			}
+		}
+		int minLetters = cities[0].length();
+		for (int i = 0; i < cities.length; ++i) {
+			if (minLetters > cities[i].length()) {
+				minLetters = cities[i].length();
+
+			}
+		}
+		for (int i = 0; i < cities.length; i++) {
+			if (cities[i].length() == minLetters) {
+				System.out.println(cities[i] + " - имеет меньше всего букв.");
+			}
+		}
+	}
 }
