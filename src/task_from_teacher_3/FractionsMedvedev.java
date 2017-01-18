d9c5cd5c062ed17f0ffe3d1c7a1a5a43827e9d5a
Yakov Medvedev <medvedev.teror@gmail.com>
2017-01-18 13:26:44 +0200

    Task №3 commit 4.
    Changed methods for using with a single parameter.

--- src/task_from_teacher_3/FractionsMedvedev.java
+++ src/task_from_teacher_3/FractionsMedvedev.java
@@ -53,56 +53,68 @@ public class FractionsMedvedev {
 		_fractionPart = (short) Math.abs(fractionPart);
 	}
 	
-	// Данный метод сумирует 2 числа типа FractionsMedvedev.
-	public void summ(FractionsMedvedev a, FractionsMedvedev b) {
-		long commonDenominator = a.getFractionPart() * b.getFractionPart();
-		long intPartA = (commonDenominator / a.getFractionPart()) * a.getIntegerPart();
-		long intPartB = (commonDenominator / b.getFractionPart()) * b.getIntegerPart();
-		;
-		long numeratorPartSumm = intPartA + intPartB;
+	// Данный метод суммирует 2 числа типа FractionsMedvedev.
+	public void summ(FractionsMedvedev someNumber) {
+		long commonDenominator;
+		long numeratorPartSumm;
+		if (this.getFractionPart() == someNumber.getFractionPart()){
+			 commonDenominator = this.getFractionPart();
+			 numeratorPartSumm = this.getIntegerPart() + someNumber.getIntegerPart();
+		} else {
+		commonDenominator = this.getFractionPart() * someNumber.getFractionPart();
+		long intPartA = (commonDenominator / someNumber.getFractionPart()) * someNumber.getIntegerPart();
+		long intPartB = (commonDenominator / this.getFractionPart()) * this.getIntegerPart();
+		numeratorPartSumm = intPartA + intPartB;
+		}
 		double resultInDouble = (double) numeratorPartSumm / (double) commonDenominator;
-		System.out.print(a.getIntegerPart() + "/" + a.getFractionPart() + " + " +
-						 b.getIntegerPart() + "/"+ b.getFractionPart() + " = ");
+		System.out.print(someNumber.getIntegerPart() + "/" + someNumber.getFractionPart() + " + " +
+				this.getIntegerPart() + "/"+ this.getFractionPart() + " = ");
 		System.out.println(numeratorPartSumm + "/" + commonDenominator + " или " + resultInDouble);
 	}
 
 	// Данный метод вычитает 2 числа типа FractionsMedvedev.
-	public void subtraction(FractionsMedvedev a, FractionsMedvedev b) {
-		long commonDenominator = a.getFractionPart() * b.getFractionPart();
-		long intPartA = (commonDenominator / a.getFractionPart()) * a.getIntegerPart();
-		long intPartB = (commonDenominator / b.getFractionPart()) * b.getIntegerPart();
-		;
-		long numeratorPartSumm = intPartA - intPartB;
-		double resultInDouble = (double) numeratorPartSumm / (double) commonDenominator;
-		System.out.print(a.getIntegerPart() + "/" + a.getFractionPart() + " - " + b.getIntegerPart() + "/"
-				+ b.getFractionPart() + " = ");
-		System.out.println(numeratorPartSumm + "/" + commonDenominator + " или " + resultInDouble);
+	public void subtraction(FractionsMedvedev someNumber) {
+		long commonDenominator;
+		long numeratorPartSubtr;
+		if (this.getFractionPart() == someNumber.getFractionPart()){
+			 commonDenominator = this.getFractionPart();
+			 numeratorPartSubtr = this.getIntegerPart() - someNumber.getIntegerPart();
+		} else {
+		commonDenominator = this.getFractionPart() * someNumber.getFractionPart();
+		long intPartA = (commonDenominator / this.getFractionPart()) * this.getIntegerPart();
+		long intPartB = (commonDenominator / someNumber.getFractionPart()) * someNumber.getIntegerPart();
+		numeratorPartSubtr = intPartA - intPartB;
+		}
+		double resultInDouble = (double) numeratorPartSubtr / (double) commonDenominator;
+		System.out.print(this.getIntegerPart() + "/" + this.getFractionPart() + " - " + someNumber.getIntegerPart() + "/"
+				+ someNumber.getFractionPart() + " = ");
+		System.out.println(numeratorPartSubtr + "/" + commonDenominator + " или " + resultInDouble);
 	}
 
 	// Данный метод умножает 2 числа типа FractionsMedvedev.
-	public void multiplication(FractionsMedvedev a, FractionsMedvedev b) {
-		long multNumerators = a.getIntegerPart() * b.getIntegerPart();
-		long multDenominators = a.getFractionPart() * b.getFractionPart();
+	public void multiplication(FractionsMedvedev someNumber) {
+		long multNumerators = this.getIntegerPart() * someNumber.getIntegerPart();
+		long multDenominators = this.getFractionPart() * someNumber.getFractionPart();
 		double resultInDouble = (double) multNumerators / (double) multDenominators;
 		if (multNumerators > multDenominators) {
 			long intQuantity = (multNumerators / multDenominators);
 			long numerator = multNumerators - (intQuantity * multDenominators);
-			System.out.print(a.getIntegerPart() + "/" + a.getFractionPart() + " * " + b.getIntegerPart() + "/"
-					+ b.getFractionPart() + " = ");
+			System.out.print(this.getIntegerPart() + "/" + this.getFractionPart() + " * " + someNumber.getIntegerPart() + "/"
+					+ someNumber.getFractionPart() + " = ");
 			System.out.println(intQuantity + " " + numerator + "/" + multDenominators + " или " + resultInDouble);
 		} else {
-			System.out.print(a.getIntegerPart() + "/" + a.getFractionPart() + " * " + b.getIntegerPart() + "/"
-					+ b.getFractionPart() + " = ");
+			System.out.print(this.getIntegerPart() + "/" + this.getFractionPart() + " * " + someNumber.getIntegerPart() + "/"
+					+ someNumber.getFractionPart() + " = ");
 			System.out.println(multNumerators + "/" + multDenominators + " или " + resultInDouble);
 		}
 	}
 
 	// Данный метод сравнивает 2 числа типа FractionsMedvedev.
-	public void comparison(FractionsMedvedev a, FractionsMedvedev b) {
-		System.out.print(a.getIntegerPart() + "/" + a.getFractionPart() + " и " + b.getIntegerPart() + "/"
-				+ b.getFractionPart() + " - ");
-		double numerator = (double) a.getIntegerPart() / (double) a.getFractionPart();
-		double denominator = (double) b.getIntegerPart() / (double) b.getFractionPart();
+	public void comparison(FractionsMedvedev someNumber) {
+		System.out.print(this.getIntegerPart() + "/" + this.getFractionPart() + " и " + someNumber.getIntegerPart() + "/"
+				+ someNumber.getFractionPart() + " - ");
+		double numerator = (double) this.getIntegerPart() / (double) this.getFractionPart();
+		double denominator = (double) someNumber.getIntegerPart() / (double) someNumber.getFractionPart();
 		if (numerator == denominator) {
 			System.out.println("числа равны");
 		} else if (numerator > denominator) {
--- src/task_from_teacher_3/Main.java
+++ src/task_from_teacher_3/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		FractionsMedvedev testVar = new FractionsMedvedev();
-		FractionsMedvedev a = new FractionsMedvedev(112 ,(short) 43);
+		
+		FractionsMedvedev a = new FractionsMedvedev(112 ,(short) 46);
 		FractionsMedvedev b = new FractionsMedvedev(111,(short) 43);
 
-		testVar.summ(a, b);
-		testVar.subtraction(a, b);
-		testVar.multiplication(a, b);
-		testVar.comparison(a, b);
+		a.summ(b);
+		a.subtraction(b);
+		a.multiplication(b);
+		a.comparison(b);
 	}
 }
